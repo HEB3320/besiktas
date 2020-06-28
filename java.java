d895522c2716b9d5eba901e91fb2f264c0aff4cc
Sam Smith <sam@example.com>
2020-06-28 19:56:09 -0400

    first commit

---
+++ java.java
@@ -0,0 +1,12 @@
+ Volume in drive C is OS
+ Volume Serial Number is 9E0A-3E18
+
+ Directory of C:\Users\engin\Desktop\bjk
+
+06/28/2020  07:53 PM    <DIR>          .
+06/28/2020  07:53 PM    <DIR>          ..
+06/28/2020  07:53 PM               392 aylin.txt
+06/28/2020  07:52 PM             4,249 engin.txt
+06/28/2020  07:53 PM                 0 java.java
+               3 File(s)          4,641 bytes
+               2 Dir(s)  33,023,090,688 bytes free
